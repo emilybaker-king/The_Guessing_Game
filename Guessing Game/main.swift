0e7c56dfe34aea853087be17d0b36c863013286a
Emily Baker-King <emilybaker-king@interapts-macbook-pro-102.local>
2018-08-16 10:21:57 -0500

    Initial Commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Emily Baker-King on 8/16/18.
+//  Copyright © 2018 Emily Baker-King. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
