e3efea6ca5c463a8bd49a433a01b75a5be13001d
Emily Baker-King <emilybaker-king@interapts-macbook-pro-102.local>
2018-08-20 12:49:49 -0500

    add all files

---
+++ If statements.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+// If statements
+
+var inClass = true
+
+if inClass == true {
+ print("Hey we're in class")
+} else {
+    print("Aww, we're not in class")
+}
+
+
+/*
+ if <condition> {
+ print ("")
+ }
+ */
+
